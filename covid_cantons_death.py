756765019c039e256136dc9834c41f0403fe702d
Mark Walther <ma-wa@gmx.ch>
2020-10-30 16:54:10 +0100

    auto-update

--- covid_cantons_death.py
+++ covid_cantons_death.py
@@ -1,50 +1,55 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[38]:
+# In[2]:
 
 
 import pandas as pd
 import general_settings
+from time import sleep
 
 
-# In[39]:
+# In[14]:
 
 
+def death_getter(canton):
 #get data from github
-df = pd.read_csv("https://raw.githubusercontent.com/daenuprobst/covid19-cases-switzerland/master/covid19_cases_fatalities_switzerland_bag.csv")
+    df = pd.read_csv("https://raw.githubusercontent.com/daenuprobst/covid19-cases-switzerland/master/covid19_cases_fatalities_switzerland_bag.csv")
 
-#formatting
-df["date_new"] = pd.to_datetime(df["date"], format="%Y-%m-%d")
-df.pop("date")
-df.set_index("date_new", inplace=True)
-df.rename_axis(None, inplace=True)
+    #formatting
+    df["date_new"] = pd.to_datetime(df["date"], format="%Y-%m-%d")
+    df.pop("date")
+    df.set_index("date_new", inplace=True)
+    df.rename_axis(None, inplace=True)
+    
+    #choose canton
+    if canton == "OCH":
+        df_ag = df[(df["canton"] == "SG") | (df["canton"] == "TG") | (df["canton"] == "AR") | (df["canton"] == "AI")  | (df["canton"] == "SH")]
+    elif canton == "ZCH":
+        df_ag = df[(df["canton"] == "LU") | (df["canton"] == "SZ") | (df["canton"] == "UR") | (df["canton"] == "OW")  | (df["canton"] == "NW")]
+    
+    else:
+        df_ag = df[df["canton"] == canton]
+    df_ag2 = df_ag.drop(["canton", "cases"], axis=1)
+    
+    #groupby age
+    s = df_ag2.groupby("age_group")["fatalities"].sum()
+    df_final = pd.DataFrame(s)
+    df_final.index.name = "Altersklasse"
+    df_final.columns = ["Todesfälle"]
+    #export backup to csv
+    df_final.to_csv("/root/covid_aargau/backups/death/todesfaelle_{}_{}.csv".format(canton, general_settings.today))
+
+    #export to csv
+    df_final.to_csv("/root/covid_aargau/data/deaths_{}.csv".format(canton))
 
 
-# In[40]:
+# In[17]:
 
 
-#choose AG
-df_ag = df[df["canton"] == "AG"]
-df_ag2 = df_ag.drop(["canton", "cases"], axis=1)
+cantons = ["AG", "OCH", "ZCH"]
 
-
-# In[46]:
-
-
-#groupby age
-s = df_ag2.groupby("age_group")["fatalities"].sum()
-df_final = pd.DataFrame(s)
-df_final.index.name = "Altersklasse"
-df_final.columns = ["Todesfälle"]
-
-
-# In[49]:
-
-
-#export backup to csv
-df_final.to_csv("/root/covid_aargau/backups/death/todesfaelle_AG_{}.csv".format(general_settings.today))
-
-#export to csv
-df_final.to_csv("/root/covid_aargau/data/deaths.csv")
+for canton in cantons:
+    death_getter(canton)
+    sleep(5)
 

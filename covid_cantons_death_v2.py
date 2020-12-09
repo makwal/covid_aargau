7a9512db4b228166896d39da25bcd76855fa2974
Mark Walther <ma-wa@gmx.ch>
2020-12-09 10:42:12 +0100

    auto-update

---
+++ covid_cantons_death_v2.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#in Betrieb ab 9. 12. 2020
+import pandas as pd
+import requests
+from time import sleep
+from general_settings import backdate
+from datetime import timedelta
+
+
+# In[ ]:
+
+
+base_url = "https://www.covid19.admin.ch/api/data/context"
+
+
+# In[ ]:
+
+
+r = requests.get(base_url)
+response = r.json()
+files = response["sources"]["individual"]
+url1 = files["csv"]["weekly"]["byAge"]["death"]
+df_import = pd.read_csv(url1)
+
+
+# In[ ]:
+
+
+def death(canton):
+    #get canton
+    df = df_import[(df_import["geoRegion"] == canton)]
+    
+    #make list of all age categories
+    age_list = df["altersklasse_covid19"].unique()
+
+    
+    #get last row of each age category, concat to df_final
+    df_final = pd.DataFrame([])
+
+    for a in age_list:
+        row = df[df["altersklasse_covid19"] == a].tail(1)[["altersklasse_covid19", "sumTotal"]]
+        df_final = pd.concat([df_final, row])
+        
+    #export backup to csv
+    df_final.to_csv("/root/covid_aargau/backups/schnelltests/death_{}_{}.csv".format(canton, backdate(0)), index=False)
+    
+    #export to csv
+    df_final.to_csv("/root/covid_aargau/data/death_{}.csv".format(canton), index=False)
+
+
+# In[ ]:
+
+
+cantons = ["AG", "SG", "AI", "AR", "TG", "LU", "ZG", "SZ", "OW", "NW", "UR"]
+
+for canton in cantons:
+    death(canton)
+    sleep(5)
+

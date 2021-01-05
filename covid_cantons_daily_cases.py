220732d1c55c31cf0eca37802f898f88b766112b
Mark Walther <ma-wa@gmx.ch>
2021-01-05 09:38:39 +0100

    auto-update

---
+++ covid_cantons_daily_cases.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import requests
+from time import sleep
+from general_settings import backdate
+from datetime import timedelta
+
+
+# In[2]:
+
+
+base_url = "https://www.covid19.admin.ch/api/data/context"
+
+
+# In[3]:
+
+
+r = requests.get(base_url)
+response = r.json()
+files = response["sources"]["individual"]
+url1 = files["csv"]["daily"]["cases"]
+df_import = pd.read_csv(url1)
+
+
+# In[9]:
+
+
+def daily_cases(canton):    
+    
+    #get canton and relevant columns
+    df = df_import[(df_import["geoRegion"] == canton)][["datum", "entries"]]
+
+    #export backup to csv
+    df.to_csv("/root/covid_aargau/backups/daily_cases/daily_cases_{}_{}.csv".format(canton, backdate(0)), index=False)
+    
+    #export to csv
+    df.to_csv("/root/covid_aargau/data/daily_cases/daily_cases_{}.csv".format(canton), index=False)
+
+
+# In[ ]:
+
+
+cantons = ["SG", "AI", "AR", "TG", "LU", "ZG", "SZ", "OW", "NW", "UR"]
+
+for canton in cantons:
+    daily_cases(canton)
+    sleep(5)
+

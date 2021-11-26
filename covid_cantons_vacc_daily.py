bd98d7cc01601e4ff494cd8ceda1196802445b5f
Mark Walther <ma-wa@gmx.ch>
2021-11-26 14:34:02 +0100

    auto-update

--- covid_cantons_vacc_daily.py
+++ covid_cantons_vacc_daily.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[3]:
 
 
 import pandas as pd
@@ -11,7 +11,7 @@ from time import sleep
 from datetime import datetime, timedelta
 
 
-# In[2]:
+# In[4]:
 
 
 #url BAG
@@ -22,7 +22,7 @@ datawrapper_url = 'https://api.datawrapper.de/v3/charts/'
 headers = {'Authorization': datawrapper_api_key}
 
 
-# In[3]:
+# In[5]:
 
 
 r = requests.get(base_url)
@@ -32,7 +32,7 @@ url = files['csv']['vaccPersonsV2']
 df_import = pd.read_csv(url)
 
 
-# In[4]:
+# In[6]:
 
 
 def vacc_daily_cantons(canton):
@@ -45,20 +45,20 @@ def vacc_daily_cantons(canton):
     df.set_index('date', inplace=True)
     df.rename(columns={'mean7d': 'Erstimpfungen 7-Tages-Schnitt'}, inplace=True)
     
-    #only last twelve  weeks
+    #only last twelve weeks
     end_date = df.index[-1]
     start_date = end_date - timedelta(weeks=12)
 
-    df_short = df[start_date:end_date].copy()
+    df_short = df.loc[start_date:end_date].copy()
 
     #export to csv
-    df_short.to_csv('/root/covid_aargau/data/vaccination/vacc_daily_short_{}.csv'.format(canton))
-    df.to_csv('/root/covid_aargau/data/vaccination/vacc_daily_{}.csv'.format(canton))
+    #df_short.to_csv('/root/covid_aargau/data/vaccination/vacc_daily_short_{}.csv'.format(canton))
+    #df.to_csv('/root/covid_aargau/data/vaccination/vacc_daily_{}.csv'.format(canton))
 
     return start_date, end_date
 
 
-# In[5]:
+# In[7]:
 
 
 cantons = ['AG', 'SO', 'SG', 'AI', 'AR', 'TG', 'LU', 'ZG', 'SZ', 'OW', 'NW', 'UR', 'CH']
@@ -71,7 +71,7 @@ for canton in cantons:
 
 # **Datawrapper update**
 
-# In[6]:
+# In[8]:
 
 
 cantons_dict = {
@@ -85,13 +85,14 @@ cantons_dict = {
     'UR': 'YDWRO',
     'SO_short': '5x9Kp',
     'SO': 'ag74O',
-    'CH': 'roqDv'
+    'CH_short': 'roqDv',
+    'CH': 'YPYBL'
 }
 
 
 # Create Ticks for Datawrapper
 
-# In[7]:
+# In[9]:
 
 
 ticks = []
@@ -107,7 +108,7 @@ ticks.append(end_date.strftime('%Y-%m-%d'))
 tick_string = ', '.join(ticks)
 
 
-# In[8]:
+# In[10]:
 
 
 note = '''<span style="color:#003595">Blaue Linie</span>: 7-Tage-Durchschnitt der Impfungen (+/- 3 Tage). <span style="color:#989898">Graue Balken</span>: Anzahl Impfungen pro Tag. An Sonntagen wird weniger geimpft. Die Zahlen können von den Angaben des Kantons abweichen'''
@@ -134,11 +135,11 @@ def chart_updater(chart_id, end_date, tick_string):
     res_publish = requests.post(url_publish, headers=headers)
 
 
-# In[9]:
+# In[11]:
 
 
 for canton, chart_id in cantons_dict.items():
     chart_updater(chart_id, end_date, tick_string)
-    if canton != 'AG_short' or canton != 'SO_short':
+    if canton != 'AG_short' or canton != 'SO_short' or canton != 'CH_short':
         chart_updater(chart_id, end_date, tick_string='')
 

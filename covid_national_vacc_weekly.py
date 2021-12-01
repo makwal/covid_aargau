5f125d9a94d691004a2dd5e5c7574c30a0c0a5e8
Mark Walther <ma-wa@gmx.ch>
2021-12-01 16:34:01 +0100

    auto-update

--- covid_national_vacc_weekly.py
+++ covid_national_vacc_weekly.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[ ]:
 
 
 import pandas as pd
 import requests
 from general_settings import backdate, datawrapper_api_key
 from time import sleep
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
-# In[2]:
+# In[ ]:
 
 
 #url BAG
@@ -22,7 +22,7 @@ datawrapper_url = 'https://api.datawrapper.de/v3/charts/'
 headers = {'Authorization': datawrapper_api_key}
 
 
-# In[3]:
+# In[ ]:
 
 
 r = requests.get(base_url)
@@ -32,58 +32,65 @@ url = files['csv']['vaccDosesAdministered']
 df_import = pd.read_csv(url)
 
 
-# In[4]:
+# In[ ]:
 
 
-#choose only canton and necessary columns
-df = df_import[df_import['geoRegion'] == 'CH'].copy()
-df = df[['date', 'sumTotal']].copy()
+def data_wrangler(canton):
+    #choose only canton and necessary columns
+    df = df_import[df_import['geoRegion'] == canton].copy()
+    df = df[['date', 'sumTotal']].copy()
 
-#calculate daily administered doses
-df['verabreichte Dosen'] = df['sumTotal'].diff()
+    #calculate daily administered doses
+    df['verabreichte Dosen'] = df['sumTotal'].diff()
 
-df['date'] = pd.to_datetime(df['date'])
-df.set_index('date', inplace=True)
+    df['date'] = pd.to_datetime(df['date'])
+    df.set_index('date', inplace=True)
 
-#use resampling to calulate weekly sum
-df_final = pd.DataFrame(df.resample('W')['verabreichte Dosen'].sum())
-df_final = df_final['2021-01-31':]
+    #use resampling to calulate weekly sum
+    df_final = pd.DataFrame(df.resample('W')['verabreichte Dosen'].sum())
+    df_final = df_final['2021-01-31':]
 
-#Prevent current week from appearing in dataframe
-today = datetime.today()
-df_final = df_final[df_final.index < today].copy()
+    #Prevent current week from appearing in dataframe
+    today = datetime.today()
+    df_final = df_final[df_final.index < today].copy()
+    
+    #for Datawrapper charts
+    end_date = df_final.index[-1].strftime('%d.%m.%Y')
+    start_date = df_final.index[-1] - timedelta(days=6)
+    start_date = start_date.strftime('%d.%m.')
+    latest_value = df_final['verabreichte Dosen'].tail(1).values[0].astype(int)
+    
+    example = f'Lesebeispiel: In der Woche vom {start_date} bis {end_date} wurden {latest_value} Impfungen verabreicht.'
+    date = datetime.today().date().strftime('%d.%m.%Y')
+
+    #export to csv
+    df_final.to_csv('/root/covid_aargau/data/vaccination/vacc_{}_weekly.csv'.format(canton))
+
+    return example, date
+
+
+# **Datawrapper update**
+
+# In[ ]:
+
+
+chart_ids = {
+    'CH': 'qDQY3'
+}
 
 
 # In[ ]:
 
 
-#make a backup export of the current data
-df_final.to_csv('/root/covid_aargau/backups/vacc_ch/backup_CH_weekly_{}.csv'.format(backdate(0)))
-
-#export to csv
-df_final.to_csv('/root/covid_aargau/data/vaccination/vacc_CH_weekly.csv')
-
-
-# **Datawrapper update**
-
-# In[6]:
-
-
-chart_id = 'qDQY3'
-date = datetime.today().date().strftime('%d.%m.%Y')
-
-
-# In[8]:
-
-
-def chart_updater(chart_id, date):
+def chart_updater(chart_id, example, date):
 
     url_update = datawrapper_url + chart_id
     url_publish = url_update + '/publish'
 
     payload = {
 
-    'metadata': {'describe': {'intro': f'Aktualisiert: {date}'}}
+    'metadata': {'describe': {'intro': example},
+                 'annotate': {'notes': f'Aktualisiert: {date}'}}
 
     }
 
@@ -93,6 +100,20 @@ def chart_updater(chart_id, date):
 
     res_publish = requests.post(url_publish, headers=headers)
 
-#call function
-chart_updater(chart_id, date)
+
+# In[ ]:
+
+
+def main_function(canton, chart_id):
+    example, date = data_wrangler(canton)
+    
+    chart_updater(chart_id, example, date)
+
+
+# In[ ]:
+
+
+#call main function
+for canton, chart_id in chart_ids.items():
+    main_function(canton, chart_id)
 
